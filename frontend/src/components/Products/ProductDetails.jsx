c9a595aaca1bf0cf37c12d2bca0a71f7f72950f5
quanphung27-05 <100451205+quanphung27-05@users.noreply.github.com>
2024-05-02 23:19:09 +0700

    update

--- frontend/src/components/Products/ProductDetails.jsx
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -134,6 +134,16 @@ const ProductDetails = ({ data }) => {
                 <div className="pt-3">
                   <h4 className={`${styles.productDiscountPrice} text-lg`}>
                     {formatPrice(data.discountPrice)}
+                    {data.originalPrice && data.discountPrice && (
+                      <span style={{ color: "red" }} className=" ml-2">
+                        -
+                        {(
+                          (1 - data.discountPrice / data.originalPrice) *
+                          100
+                        ).toFixed(0)}
+                        %
+                      </span>
+                    )}
                   </h4>
                   <h3 className={`${styles.price} mt-4`}>
                     {formatPrice(
@@ -261,7 +271,9 @@ const ProductDetails = ({ data }) => {
                         >
                           <path d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z"></path>
                         </svg>
-                        <p className="mb-0">{totalReviewsLength} lượt đánh giá</p>
+                        <p className="mb-0">
+                          {totalReviewsLength} lượt đánh giá
+                        </p>
                       </div>
                       <div class="flex-1 inline-flex items-center">
                         <svg
@@ -324,15 +336,15 @@ const ProductDetailsInfo = ({
 
   return (
     <div className="bg-[#f5f6fb] px-3 800px:px-10 py-2 rounded">
-      <div className="w-full flex justify-between border-b pt-10 pb-2">
-        <div className="relative">
+      <div className="w-full flex justify-start border-b pt-10 pb-2">
+        <div className="relative mr-8">
           <h5
             className={
               "text-[#000] text-[18px] px-1 leading-5 font-[600] cursor-pointer 800px:text-[20px]"
             }
             onClick={() => setActive(1)}
           >
-            Product Details
+            Chi tiết sản phẩm
           </h5>
           {active === 1 ? (
             <div className={`${styles.active_indicator}`} />
@@ -345,22 +357,9 @@ const ProductDetailsInfo = ({
             }
             onClick={() => setActive(2)}
           >
-            Product Reviews
+            Đánh giá sản phẩm
           </h5>
           {active === 2 ? (
-            <div className={`${styles.active_indicator}`} />
-          ) : null}
-        </div>
-        <div className="relative">
-          <h5
-            className={
-              "text-[#000] text-[18px] px-1 leading-5 font-[600] cursor-pointer 800px:text-[20px]"
-            }
-            onClick={() => setActive(3)}
-          >
-            Seller Information
-          </h5>
-          {active === 3 ? (
             <div className={`${styles.active_indicator}`} />
           ) : null}
         </div>
@@ -395,61 +394,11 @@ const ProductDetailsInfo = ({
 
           <div className="w-full flex justify-center">
             {data && data.reviews.length === 0 && (
-              <h5>No Reviews have for this product!</h5>
+              <h5>Sản phẩm này chưa có bất kì đánh giá nào!</h5>
             )}
           </div>
         </div>
       ) : null}
-
-      {active === 3 && (
-        <div className="w-full block 800px:flex p-5">
-          <div className="w-full 800px:w-[50%]">
-            <Link to={`/shop/preview/${data.shop._id}`}>
-              <div className="flex items-center">
-                <img
-                  src={`${data?.shop?.avatar?.url}`}
-                  className="w-[50px] h-[50px] rounded-full"
-                  alt=""
-                />
-                <div className="pl-3">
-                  <h3 className={`${styles.shop_name}`}>{data.shop.name}</h3>
-                  <h5 className="pb-2 text-[15px]">
-                    ({averageRating}/5) Ratings
-                  </h5>
-                </div>
-              </div>
-            </Link>
-            <p className="pt-2">{data.shop.description}</p>
-          </div>
-          <div className="w-full 800px:w-[50%] mt-5 800px:mt-0 800px:flex flex-col items-end">
-            <div className="text-left">
-              <h5 className="font-[600]">
-                Joined on:{" "}
-                <span className="font-[500]">
-                  {data.shop?.createdAt?.slice(0, 10)}
-                </span>
-              </h5>
-              <h5 className="font-[600] pt-3">
-                Total Products:{" "}
-                <span className="font-[500]">
-                  {products && products.length}
-                </span>
-              </h5>
-              <h5 className="font-[600] pt-3">
-                Total Reviews:{" "}
-                <span className="font-[500]">{totalReviewsLength}</span>
-              </h5>
-              <Link to="/">
-                <div
-                  className={`${styles.button} !rounded-[4px] !h-[39.5px] mt-3`}
-                >
-                  <h4 className="text-white">Visit Shop</h4>
-                </div>
-              </Link>
-            </div>
-          </div>
-        </div>
-      )}
     </div>
   );
 };
