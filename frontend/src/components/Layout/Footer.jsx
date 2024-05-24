16b4316441ace905e49aada3c0c669c089a5a41a
bytesculptors <tuannghia200603@gmail.com>
2024-05-24 11:45:50 +0700

    Change logo

--- frontend/src/components/Layout/Footer.jsx
+++ frontend/src/components/Layout/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import logo from './logo.png'
 import {
   AiFillFacebook,
   AiFillInstagram,
@@ -37,7 +38,8 @@ const Footer = () => {
       <div className="grid grid-cols-1 sm:gird-cols-3 lg:grid-cols-4 gap-6 sm:px-8 px-5 py-16 sm:text-center">
         <ul className="px-5 text-center sm:text-start flex sm:block flex-col items-center">
           <img
-            src="https://shopo.quomodothemes.website/assets/images/logo.svg"
+            // src="https://shopo.quomodothemes.website/assets/images/logo.svg"
+            src={logo}
             alt=""
             style={{ filter: "brightness(0) invert(1)" }}
           />
